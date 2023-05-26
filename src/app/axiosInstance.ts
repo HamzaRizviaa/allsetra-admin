10103da2e539921e2dd7cd0a5fd62758031faa6f
hamzahhamlet <hamzah.hamlet@gmail.com>
2023-05-26 15:19:18 +0500

    change in the interceptor of axios request

--- src/app/axiosInstance.ts
+++ src/app/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { InteractionRequiredAuthError } from "@azure/msal-browser";
 import { msalInstance } from "index";
 
 const instance = axios.create({
@@ -20,16 +21,23 @@ instance.interceptors.request.use(
       account: account,
     };
 
-    const accessTokenResponse = await msalInstance.acquireTokenSilent(
-      accessTokenRequest
-    );
+    try {
+      const accessTokenResponse = await msalInstance.acquireTokenSilent(
+        accessTokenRequest
+      );
 
-    config.headers["Authorization"] = `bearer ${accessTokenResponse.idToken}`;
+      config.headers["Authorization"] = `bearer ${accessTokenResponse.idToken}`;
 
-    config.headers["X-Subscription"] =
-      process.env.REACT_APP_API_HEADER_SUBSCRIPTION;
-
-    return config;
+      config.headers["X-Subscription"] =
+        process.env.REACT_APP_API_HEADER_SUBSCRIPTION;
+    } catch (error) {
+      if (error instanceof InteractionRequiredAuthError) {
+        msalInstance.acquireTokenRedirect(accessTokenRequest);
+      }
+      console.log(error);
+    } finally {
+      return config;
+    }
   },
   function (error) {
     // Do something with request error
